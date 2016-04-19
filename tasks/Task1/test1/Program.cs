ad9ccc3ab34cc9ed1be93e916302e3aef9c191d9
joalra <johannes.rasinger@gmx.at>
2016-04-19 19:13:58 +0200

    task1 - hello world

---
+++ tasks/Task1/test1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i = 0; i < 10; i++)
+            {
+                Console.WriteLine("test {0}",i);
+                Console.WriteLine($"test {i*i}", i);
+            }
+        }
+    }
+}
