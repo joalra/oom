6e326c30a5ef190b9daf8492f68d0bf286a98446
joalra <johannes.rasinger@gmx.at>
2016-04-26 18:58:42 +0200

    implemented task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoccerPlayer origi = new SoccerPlayer("Divock Origi", "");
+            origi.setTeam("Liverpool FC");
+            origi.MarketValueM = 9;
+            origi.Position = "Forward";
+            Console.WriteLine(origi);
+
+        }
+    }
+}
---
+++ tasks/Task2/Task2/SoccerPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class SoccerPlayer
+    {
+        private string name;
+        private string team;
+        private decimal _MarketValueM;
+        private string _position;
+
+        public SoccerPlayer(string name, string team)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new Exception("emty name");
+            this.name = name;
+            setTeam(team);
+        }
+
+        public void setTeam(string team)
+        {
+            this.team = team;
+        }
+        public string getTeam()
+        {
+            return this.team;
+        }
+
+        public string getName() => name;
+
+        public decimal MarketValueM
+        {
+            get { return _MarketValueM; }
+            set { _MarketValueM = value; }
+        }
+
+        public string Position { get; set; }
+
+        public override string ToString()
+        {
+            return "|name| " + name + " |team| " + team + " |market value| "
+                + _MarketValueM + "m |position| " + Position;  
+        }
+    }
+}
