94da803afeb39f166f6c1c0c76ca29667be084b2
joalra <johannes.rasinger@gmx.at>
2016-05-10 18:01:24 +0200

    copy task2 to task3

---
+++ tasks/Task3/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoccerPlayer origi = new SoccerPlayer("Divock Origi", "Liverpool U21");
+            origi.Position = "Forward";
+            Console.WriteLine(origi);
+            origi.setTeam("Liverpool FC");
+            origi.MarketValueM = 9;
+            Console.WriteLine(origi);
+
+        }
+    }
+}
