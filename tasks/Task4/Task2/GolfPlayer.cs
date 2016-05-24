b6b8fd7a1e19f9b0700393fb6572826e2b1a6b60
joalra <johannes.rasinger@gmx.at>
2016-05-24 18:03:13 +0200

    Klasse deserialisieren

---
+++ tasks/Task4/Task2/GolfPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class GolfPlayer
+    {
+        public GolfPlayer(string name, string nation, int worldRank)
+        {
+            Name = name;
+            Nation = nation;
+            WorldRank = worldRank;
+        }
+        public string Name { get; set; }
+        public string Nation { get; set; }
+        public int WorldRank { get; set; }
+    }
+}
